0298ce7abeaa49a28f90952d0dac0086960cf4b6
TonyWu20 <tony.w21@gmail.com>
2026-09-16 00:26:24 +0800

    flake: bump `rushi-common` hash

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
             nativeBuildInputs = [ rustToolchain ];
             cargoLock = {
               lockFile = ./Cargo.lock;
-              outputHashes = { "ratatui-markdown-0.3.6" = "sha256-++qk2uLCBvak22vQf2OmGta5YaLtHCKiKAx4wLVg7yk="; "rushi-common-0.1.0" = "sha256-aAESt56XM4fulN+aYdm/+EauKZqgj0RiNoD1Dzza46A="; };
+              outputHashes = { "ratatui-markdown-0.3.6" = "sha256-++qk2uLCBvak22vQf2OmGta5YaLtHCKiKAx4wLVg7yk="; "rushi-common-0.1.0" = "sha256-g7irE4aywgvvae/E14W4kjhX/RFuRpHeqCp1l+HsvlU="; };
             };
             # Build only the `tui` binary (and its path-dep
             # `tui-highlight`); skip the standalone `tui-stream-drt`
